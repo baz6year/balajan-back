b7a13245bc948fd48c5982114e5153708189a366
alimanbazarbekova <aliman.bazarbekova@alatoo.edu.kg>
2025-11-17 13:38:40 +0600

    Initial commit: Spring Boot project

---
+++ src/main/java/com/example/balajan_back/BalajanBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.balajan_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalajanBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalajanBackApplication.class, args);
+	}
+
+}
