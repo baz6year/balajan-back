5e2bbd2e0ba5426f15517d57c254cc069fdcfe3b
alimanbazarbekova <aliman.bazarbekova@alatoo.edu.kg>
2025-11-21 12:01:07 +0600

    fixed repositories for contest and user

--- src/main/java/com/example/balajan_back/repository/ContestRepository.java
+++ src/main/java/com/example/balajan_back/repository/ContestRepository.java
@@ -1,9 +1,13 @@
 package com.example.balajan_back.repository;
 
 import com.example.balajan_back.entity.Contest;
+import com.example.balajan_back.entity.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ContestRepository extends JpaRepository<Contest, Integer> {
+public interface ContestRepository extends JpaRepository<Contest, Long> {
+    Optional<Contest> findBySlug(String slug);
 }
