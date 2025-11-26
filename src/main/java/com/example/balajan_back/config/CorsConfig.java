b055c39367b1a97c44b2205baf93ada3bbe8f25e
TElzada <elzada.turdubekova@alatoo.edu.kg>
2025-11-26 19:46:46 +0600

    changed CorsConfig.java

--- src/main/java/com/example/balajan_back/config/CorsConfig.java
+++ src/main/java/com/example/balajan_back/config/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:63342")
+                .allowedOrigins(
+                        "http://localhost:63342",
+                        "https://kyialgakadam.netlify.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
